d6c94b4aa61240dd758255e301554076e1623aa4
kaylamuraoka <kmurs98@gmail.com>
2021-02-10 13:43:39 -1000

    adding comments for readability to the html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,17 @@
 const path = require("path");
 
 module.exports = function (app) {
+  // route that calls the the home page
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
+  // route for the exercise page
   app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
   });
 
+  // route for the stats page
   app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
   });
