a5b4038e947cb8cfda7c991c14add0ae37146581
kaylamuraoka <kmurs98@gmail.com>
2021-02-10 12:28:41 -1000

    adding connection to workout database in server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES - requiring necessary npm packages
 const express = require("express");
+const { Mongoose } = require("mongoose");
 const logger = require("morgan");
 
 // Setting up port and requiring models for syncing
@@ -13,6 +14,13 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
+// MONGODB CONNECTION
+Mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
 
 // ROUTES
 app.get("/", (req, res) => {
