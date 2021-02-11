c2c6d8a0f6bd840688f6a2503e13fd165249b76a
kaylamuraoka <kmurs98@gmail.com>
2021-02-10 14:25:32 -1000

    adding GET Route that calls workout data from API

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,7 +4,15 @@ module.exports = function (app) {
   // --------------------------------------------------------------
   // GET Route that calls workout data from API
   // API is called in the front-end's "getLastWorkout()" function
-  app.get("/api/workouts", (req, res) => {});
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
   // --------------------------------------------------------------
   // PUT Route to update workout data
