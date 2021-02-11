bbe589e76f962bd39f79069706188131cb5ba0cb
kaylamuraoka <kmurs98@gmail.com>
2021-02-10 18:03:30 -1000

    adding console logs to api-routes for dev purposes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,10 +6,19 @@ module.exports = function (app) {
   // API is called in the front-end's "getLastWorkout()" function
   app.get("/api/workouts", (req, res) => {
     db.Workout.find({})
+      .aggregate([
+        {
+          $addFields: {
+            totalDuration: { $sum: ["$exercises.duration"] },
+          },
+        },
+      ])
       .then((dbWorkout) => {
+        console.log(dbWorkout);
         res.json(dbWorkout);
       })
       .catch((err) => {
+        console.log(err);
         res.status(400).json(err);
       });
   });
@@ -17,12 +26,16 @@ module.exports = function (app) {
   // --------------------------------------------------------------
   // PUT Route to update workout data
   // API is called in the front-end's "addExercise()" function
-  app.put("/api/workouts/:id", ({ body, params }, res) => {
-    db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(req.params.id, {
+      $push: { exercises: req.body },
+    })
       .then((dbWorkout) => {
+        console.log(dbWorkout);
         res.json(dbWorkout);
       })
       .catch((err) => {
+        console.log(err);
         res.status(400).json(err);
       });
   });
@@ -30,12 +43,14 @@ module.exports = function (app) {
   // --------------------------------------------------------------
   // POST Route to create a new workout
   // API is called in the front-end's "createWorkout()" function
-  app.post("/api/workouts", ({ body }, res) => {
-    db.Workout.create(body)
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
       .then((dbWorkout) => {
+        console.log(dbWorkout);
         res.json(dbWorkout);
       })
       .catch((err) => {
+        console.log(err);
         res.status(400).json(err);
       });
   });
@@ -45,11 +60,13 @@ module.exports = function (app) {
   // API is called in the front-end's "getWorkoutsInRange()" function
   app.get("/api/workouts/range", (req, res) => {
     db.Workout.find({})
-      .limit(5)
+      .limit(7)
       .then((dbWorkout) => {
+        console.log(dbWorkout);
         res.json(dbWorkout);
       })
       .catch((err) => {
+        console.log(err);
         res.status(400).json(err);
       });
   });
