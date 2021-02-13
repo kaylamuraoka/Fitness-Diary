c77cefefd07913881afe37c6a124a0dcbe5285d3
kaylamuraoka <kmurs98@gmail.com>
2021-02-12 14:32:48 -1000

    update the application's code to accommodate the MongoDB connection

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(express.static("public"));
 // MONGODB CONNECTION
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // ROUTES
